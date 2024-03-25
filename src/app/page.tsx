6eeaa7b613c5b75cc5483f9854d0b82f7721ada6
Marcus <marcus@totum.io>
2024-03-25 12:36:04 -0700

    Modified product card and contact area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import {PRODUCTS} from "@/data";
 import {Product} from "@/types";
 
 function ProductCard(props: { product: Product }) {
-  return <div className="py-7 h-2/3vh flex flex-col justify-end bg-amber-300 my-7 p-7 rounded-3xl">
+  return <div className="py-7 h-2/3vh flex flex-col justify-end border-2 my-7 p-7 rounded-3xl">
     <h4 className="text-gray-700 mb-3">{props.product.organization.toLowerCase()}</h4>
     <h2 className="text-5xl">{props.product.name.toLowerCase()}</h2>
     <p className="text-lg pt-3 pb-3">{props.product.description}</p>
@@ -24,8 +24,10 @@ export default function Home() {
           ))}
         </div>
       </section>
-      <section className="h-screen flex flex-col p-12">
-        <h1 className="text-7xl">get in touch</h1>
+      <section className="h-screen flex flex-col p-12 justify-center items-center">
+        <a href="mailto:hello@marcusbernal.es">
+          <h1 className="text-7xl underline">email me</h1>
+        </a>
       </section>
     </main>
   );
