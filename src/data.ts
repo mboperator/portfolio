3f8bb7505dc7b086e408d9b09a37f701b5a85048
Marcus <marcus@totum.io>
2024-03-29 16:33:41 -0700

    Updated copy

--- src/data.ts
+++ src/data.ts
@@ -20,7 +20,7 @@ export function getProducts(): Product[] {
           component: IlaSpecs
         },
         {
-          description: 'I\'m responsible for product management, industrial design, UI/UX, and firmware and mobile app.',
+          description: 'I\'m working on this project in tandem with my good friend, co-founder, and electrical engineer. I am responsible for product management, industrial design, UI/UX, and firmware and mobile app.',
           component: IlaImplementation
         }
       ]
@@ -51,7 +51,7 @@ export function getProducts(): Product[] {
       slug: 'odyssey-journal',
       organization: 'Totum Technologies',
       name: 'Odyssey Journal',
-      description: "The private, encrypted journal for one's walk with God.",
+      description: "The private, encrypted journal for one's walk with God. The purpose of this app was to streamline three things: recording, recalling, and reflecting on life's memories.",
       platform: ['mobile', 'web'],
       technologies: ['react-native', 'mobx'],
       coverImage: '/odyssey-journal/landing-page.png',
@@ -73,8 +73,8 @@ export function getProducts(): Product[] {
       coverImage: 'prequal-1.png',
       coverImageSize: 'contain',
       slides: [
-        { image: 'prequal-2.png', imageSize: 'contain',  description: 'From this view, General Contractors can toggle fields on and off.'},
-        { image: "prequal-3.png", imageSize: 'contain', description: 'General Contractors also see submitted Prequalification form results.'}
+        { image: "prequal-3.png", imageSize: 'contain', description: 'This product was the first of Procore\'s Preconstruction product line. With a fully configurable, multi page form with complex inputs, multiple personas, and a high number of screens spanning three products - Prequalification was one of the most complex UIs in the entire suite of construction management products at the time.'},
+        { image: 'prequal-2.png', imageSize: 'contain',  description: 'I lead a team of three frontend software engineers and two interns to implement this product from the ground up. This product was implemented using Ruby on Rails, React, React Router, and Mobx state tree.'},
       ],
     },
     {
@@ -85,23 +85,24 @@ export function getProducts(): Product[] {
       platform: ['web'],
       technologies: ['react', 'ruby-on-rails', 'mobx'],
       coverImage: '',
-      slides: []
+      slides: [
+        { image: "prequal-3.png", imageSize: 'contain', description: 'This product was originally a free offering when clients purchased Procore\'s construction financials product line.'},
+        { image: "prequal-3.png", imageSize: 'contain', description: 'My team was responsible for modernizing the UI prior to selling the product as a standalone offering.'},
+        { image: "prequal-3.png", imageSize: 'contain', description: 'We redesigned the entire product as a single page application in under a year and it brought in $7M in ARR within 7 months. '},
+        { image: "prequal-3.png", imageSize: 'contain', description: 'I was responsible for frontend architecture and implementation. I lead one frontend engineer and one intern. Bid Management is implemented using Ruby on Rails, React, React Router, and redux.'},
+      ]
     },
     {
       slug: 'project-financials',
       organization: 'Procore Technologies',
       name: 'Project Financials',
-      description: 'A solution that makes it easier to collaboratively manage accurate budgets in one, accurate source of truth.',
+      description: 'A solution that makes it easier to collaboratively manage accurate budgets in one, accurate source of truth. This app was implemented ',
       platform: ['web'],
       technologies: ['react', 'ruby-on-rails', 'redux'],
       coverImage: 'project-financials.png',
       coverImageAnchor: 'left',
       coverImageSize: 'contain',
-      slides: [{
-        image: 'project-financials.png',
-        imageAnchor: 'left',
-        imageSize: 'contain'
-      }],
+      slides: [],
     },
     {
       slug: 'core',
