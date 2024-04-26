f08146009d4c60d4bf0542c12d2519405b398357
Marcus <marcus@totum.io>
2024-04-26 15:24:51 -0700

    Remove console log from server

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -8,10 +8,8 @@ const openai = new OpenAI({
 });
 
 export async function POST(req: Request) {
-  // Extract the `messages` from the body of the request
   const { messages } = await req.json();
 
-  // Request the OpenAI API for the response based on the prompt
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo-0125',
     stream: true,
@@ -35,10 +33,6 @@ export async function POST(req: Request) {
     ],
   });
 
-  console.info('Streaming', response)
-  // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
-
-  // Respond with the stream
   return new StreamingTextResponse(stream);
 }
